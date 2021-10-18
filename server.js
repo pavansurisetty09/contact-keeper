05baa9d3064876b680c2f925d0ddc50330faa11d
rishirhyspavan <surisettypavankumar999@gmail.com>
2021-10-18 16:37:59 +0530

    initial commit

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
 
-//Server static assts in production
+//Server static assets in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static("client/build"));
